6459b9f91949088d26167492d24062c1c17e2519
prolo <prolofresh@gmail.com>
2024-04-19 18:35:52 -0400

    Updated Version

--- Common/CharaMorpher.cs
+++ Common/CharaMorpher.cs
@@ -89,7 +89,7 @@ namespace Character_Morpher
 		// Avoid changing GUID unless absolutely necessary. Plugins that rely on your plugin will no longer recognize it, and if you use it in function controllers you will lose all data saved to cards before the change!
 		public const string ModName = "Character Morpher";
 		public const string GUID = "prolo.chararmorpher";//never change this
-		public const string Version = "1.1.0";
+		public const string Version = "1.2.0";
 
 		public const string strDiv = ":";
 		public const string defaultStr = "(Default)" + strDiv;
