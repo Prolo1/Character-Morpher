c848f3e60d6f2f05e31bf502d95d883fd34debab
Prolo <100950917+Prolo1@users.noreply.github.com>
2024-10-03 14:28:46 -0400

    how about a little more

---
+++ [RG] Character Morpher/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Character_Morpher;
+
+[assembly: AssemblyTitle("RG Character Morpher")]
+[assembly: AssemblyProduct("RG Character Morpher")]
+[assembly: AssemblyDescription("Morphs one character to the shape of another")]
+[assembly: AssemblyCompany("https://github.com/Prolo1/Character-Morpher")]
+[assembly: AssemblyVersion(CharaMorpher_Core.Version)]
